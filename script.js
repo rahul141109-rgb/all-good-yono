ba5e36e50cf97a8715eb523b8665ba7f10a370bc
rahul141109-rgb <rahul141109@gmail.com>
2026-08-30 03:07:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,406 @@ const apps = [
     description: "Useful Android application.",
     link: "https://www.joyrummyon.com/?code=J5K5FS3UR4F&t=1786171415"
   }
-
+{
+  name: "Boss Rummy",
+  icon: "dummy04.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.bossrummyo.com/?code=LSHNAXS6YBV&t=1786125483"
+},
+{
+  name: "Club Inr",
+  icon: "dummy05.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://clubinrvip1.one/?code=9VCY4SZJVY8&t=1786125586"
+},
+{
+  name: "Game Rummy,
+  icon: "dummy06.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://gamerummyn.com/?code=Q6WVWU7DB67&t=1786125689"
+},
+{
+  name: "Gogo Rummy",
+  icon: "dummy07.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.rummegogo.com/?code=V4UZKAGAMZ4&t=1786125758"
+},
+{
+  name: "Hi Rummy",
+  icon: "dummy08.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://joinhirummy.cc/?code=RX35BBZ7KMR&t=1786126378"
+},
+{
+  name: "Hindi 777",
+  icon: "dummy09.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.hindi777agent2.com/?code=7LF1B2TNZKQ&t=1786127210"
+},
+{
+  name: "Ind Club",
+  icon: "dummy10.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://indclub92.com/?code=34UV5E995R6&t=1786127284"
+},
+{
+  name: "Ind Rummy",
+  icon: "dummy11.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://uonoindrummyvip.com/?code=2BARB6UGYW8&t=1786127341"
+},
+{
+  name: "Ind Slots",
+  icon: "dummy12.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.indslotsreferral.com/?code=EYM1LP7JBYC&t=1786127389"
+},
+{
+  name: "Inr Rummy",
+  icon: "dummy13.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.inrrummy.vip/?code=JMQSQ1RUVWP&t=1786127486"
+},
+{
+  name: "Jaiho 777",
+  icon: "dummy14.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://jaiho77793.com/?code=GC2RKXAWJPQ&t=1786130278"
+},
+{
+  name: "Jaiho 91",
+  icon: "dummy15.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.jaiho91agent.com/?code=C42LBXNJFEZ&t=1786130332"
+},
+{
+  name: "Mahagames",
+  icon: "dummy16.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://z-mahagames.com/?code=J241P8ZECLT&t=1786130962"
+},
+{
+  name: "Jaiho Spin",
+  icon: "dummy17.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://5jaihospinwin.com/?code=7TA2UK2ZRTA&t=1786171303"
+},
+{
+  name: "Spin Gold",
+  icon: "dummy18.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://spingoldvipagent.cc/?code=HLTNL6DN45P&t=1786171544"
+},
+{
+  name: "Spin 101",
+  icon: "dummy19.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://spin101-s.com/?code=P4CCQQAZUU1&t=1786171712"
+},
+{
+  name: "Yono 777",
+  icon: "dummy20.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://uonoagentnew.asia/?code=F9MHXY5G49Q&t=1786173815"
+},
+{
+  name: "DhanWin",
+  icon: "dummy21.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.dhanwin.app?code=47TDVUHNZCG&t=1786175386"
+},
+{
+  name: "Love Rummy",
+  icon: "dummy22.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.loverummy3.com/?code=AFCWSAWTDWF&t=1786175683"
+},
+{
+  name: "Jaiho Win",
+  icon: "dummy23.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.jaihowin19.com/?code=1XNQ8YJA54U&t=1786176033"
+},
+{
+  name: "Jaiho Slots",
+  icon: "dummy24.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.jaihoslots17.com/?code=EGPRHTW34RD&t=1786175820"
+},
+{
+  name: "Max Rummy",
+  icon: "dummy25.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.maxrummy222.com/?code=QUM8SXDWU8J&t=1786176586"
+},
+{
+  name: "MBM BET",
+  icon: "dummy26.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.mbmbet15.com/?code=UPHQ3TKH3R2&t=1786176708"
+},
+{
+  name: "Neta Vip",
+  icon: "dummy27.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.neta1.vip/?code=DR08H41Y7WU&t=1786176817"
+},
+{
+  name: "Ok Rummy",
+  icon: "dummy28.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.okrummy23.com/?code=27BSYYP3G6B&t=1786176947"
+},
+{
+  name: "Rumble Rummy",
+  icon: "dummy29.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.rumblerummy555.net/?code=UC08UD8EYJG&t=1786177028"
+},
+{
+  name: "Rummy91",
+  icon: "dummy30.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.rummy91ff.com/?code=4KTACTR8HZS&t=1786177486"
+},
+{
+  name: "Rummy Ludo",
+  icon: "dummy31.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.rummyludo1.com/?code=UWPF1DF9RQB&t=1786178140"
+},
+{
+  name: "Rummy77",
+  icon: "dummy32.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://rummy77online.vip/?code=F3VEP9DHMPU&t=1786178281"
+},
+{
+  name: "Saga Slots",
+  icon: "dummy33.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.sagaslots4.com/?code=0QHJUVVTYQY&t=1786178821"
+},
+{
+  name: "Share Slots",
+  icon: "dummy34.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://share0022.com/?code=YAZ491U1BYL&t=1786178948"
+},
+{
+  name: "Slots Spin",
+  icon: "dummy35.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.slotsspinj.com/?code=XJBYDJNRCJN&t=1786179054"
+},
+{
+  name: "Slots Winner",
+  icon: "dummy36.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://slotswinnerj.com/?code=K4ERVN85SL4&t=1787517840"
+},
+{
+  name: "Spin Crush",
+  icon: "dummy37.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://cklfjeselfs.safelyearnmillionsbysharingonepersonaqfxzqyj9.com/?code=52SYM4MYNCG&t=1786179468"
+},
+{
+  name: "ABC Rummy",
+  icon: "dummy38.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.44abcrummy.com/?code=75CJM4DNAWT&t=1786125254"
+},
+{
+  name: "Top Rummy",
+  icon: "dummy39.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.toprummy.xyz/?code=7K941HHAFF1&t=1786179840"
+},
+{
+  name: "Win Rummy",
+  icon: "dummy40.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.winrummy14.com/?code=8JTJT7UMT75&t=1786180219"
+},
+{
+  name: "YN777",
+  icon: "dummy41.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.yn077.com/?code=4SWMCSQ6YTU&t=1786180375"
+},
+{
+  name: "App 42",
+  icon: "dummy42.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://example.com/app42"
+},
+{
+  name: "Yono Arcade",
+  icon: "dummy43.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://yonoarcadeapk12.com/?code=F55CSEYSN55&t=1786180580"
+},
+{
+  name: "Yono Games",
+  icon: "dummy44.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://youonogamerewards.com/?code=QPY53D2L&t=1786180777"
+},
+{
+  name: "Yono Rummy",
+  icon: "dummy45.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://yonorummy049.com/?code=U9VA76BP&t=1786181525"
+},
+{
+  name: "Yono Slots",
+  icon: "dummy46.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.yonouono.com/?code=PJBDYP2US1K&t=1787768186"
+},
+{
+  name: "Yono vip",
+  icon: "dummy47.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://yonovipbonus.vip/?code=9U8GFRKA9JZ&t=1786181897"
+},
+{
+  name: "789 Jackpot",
+  icon: "dummy48.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://join789jackpots1.com/?code=J7Z1QAFF46V&t=1786182278"
+},
+{
+  name: "App 49",
+  icon: "dummy49.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://example.com/app49"
+},
+{
+  name: "Spin 777",
+  icon: "dummy50.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://spin777ee.com/?code=YLWPH1AVJS5&t=1785509027"
+},
+{
+  name: "777.Game",
+  icon: "dummy51.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://www.777game3.com/?code=H53SXPD9DN2&t=1786617233www.777game7.com"
+},
+{
+  name: "567 Slots",
+  icon: "dummy52.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "https://join567slots.com/?code=9uxX5ER27EK&t=1787519052"
+},
+{
+  name: "Jaiho Rummy",
+  icon: "dummy53.jpg",
+  rating: "4.5",
+  version: "1.0.0",
+  description: "Useful Android application.",
+  link: "jaihorummyagent.club/?code=E74J6HRPY2T&t=1788039327"
+}
   // Isi format mein baaki apps add honge.
 ];
 
